f897a2f6bd99f4992c5ecf3c5926d667e7e6979e
Sajib Hossain <122553771+sajib9090@users.noreply.github.com>
2023-08-25 11:54:20 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,95 @@
-const inp = document.getElementById("inp");
-const val = document.getElementById("val");
+const inputValue = document.getElementById("inp");
+const valBtn = document.getElementById("val");
 
-const value = val.addEventListener("click", () => {
-  const inpV = inp.value;
-  const ratVal = parseFloat(inpV);
-  // alert(typeof ratVal + ratVal);
-  function formatNumber(num) {
-    const integerPart = Math.floor(num);
-    const decimalPart = parseFloat((num - integerPart).toFixed(1)); // Convert to a floating-point number
-    let empty = Math.floor(5 - decimalPart - integerPart);
+const value = valBtn.addEventListener("click", () => {
+  const inpValue = parseFloat(inputValue.value);
+  const result = ratingFunc(inpValue);
 
-    let output = "";
-
-    for (let i = 0; i < integerPart; i++) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star100%25png.png' />";
-    }
-
-    if (decimalPart === 0.1) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star10%25png.png' />";
-    } else if (decimalPart === 0.2) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star20%25png.png' />";
-    } else if (decimalPart === 0.3) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star30%25png.png' />";
-    } else if (decimalPart === 0.4) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star40%25png.png' />";
-    } else if (decimalPart === 0.5) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star50%25png.png' />";
-    } else if (decimalPart === 0.6) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star60%25png.png' />";
-    } else if (decimalPart === 0.7) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star70%25png.png' />";
-    } else if (decimalPart === 0.8) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star80%25png.png' />";
-    } else if (decimalPart === 0.9) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star90%25png.png' />";
-    }
-
-    for (let i = 0; i < empty; i++) {
-      output +=
-        "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star0%25png.png' />";
-    }
-
-    return output;
-  }
-
-  const num = ratVal;
-  const result = formatNumber(num);
-
-  // Render the result in an HTML element with the id "output"
   document.getElementById("star-container").innerHTML = result;
-  document.getElementById("ratings").innerHTML = num;
+  document.getElementById("rating-value").innerHTML = inpValue;
 });
 
-// alert(value);
+const ratingFunc = (value) => {
+  const integerPart = Math.floor(value);
+  const decimalPart = parseFloat((value - integerPart).toFixed(1));
+
+  let emptyPart = Math.floor(5 - decimalPart - integerPart);
+
+  let output = "";
+
+  //full star
+  for (let i = 0; i < integerPart; i++) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star100%25png.png' />";
+  }
+
+  //decimal conditional star
+  if (decimalPart === 0.1) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star10%25png.png' />";
+  } else if (decimalPart === 0.2) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star20%25png.png' />";
+  } else if (decimalPart === 0.3) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star30%25png.png' />";
+  } else if (decimalPart === 0.4) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star40%25png.png' />";
+  } else if (decimalPart === 0.5) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star60%25png.png' />";
+  } else if (decimalPart === 0.6) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star60%25png.png' />";
+  } else if (decimalPart === 0.7) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star70%25png.png' />";
+  } else if (decimalPart === 0.8) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star80%25png.png' />";
+  } else if (decimalPart === 0.9) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star90%25png.png' />";
+  }
+
+  //empty star
+  for (let i = 0; i < emptyPart; i++) {
+    output +=
+      "<img class='star-size' src='https://raw.githubusercontent.com/sajib9090/stars-resource/main/star0%25png.png' />";
+  }
+
+  return output;
+};
+
+const productRender = document.getElementById("renderProducts");
+
+productRender.addEventListener("click", () => {
+  const url = "https://dummyjson.com/products";
+  fetchingData(url);
+});
+
+const fetchingData = (url) => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => products(data.products));
+};
+
+const products = (data) => {
+  const productContainer = document.getElementById("product-container");
+  data.map((p) => {
+    const productHTML = `
+    <div class="product">
+            <img class='img-size' src=${p.thumbnail} alt="" />
+            <div class='content'>
+            <h2>${p.title}</h2>
+            <p>Previous Price: $<del>${p.price + 299}<del/></p>
+            <p>Present Price: $${p.price}</p>
+            <p>Ratings: (${p.rating})</p>
+            <p></p>
+            <div/>
+          </div>
+    `;
+    productContainer.innerHTML += productHTML;
+  });
+};
